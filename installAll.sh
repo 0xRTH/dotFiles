216966ae9f2d86649314a0ab442a22c75272d6c8
0xRTH <arthurdugon@gmail.com>
2024-04-25 16:12:45 -0400

    added installater

---
+++ installAll.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Define source and destination directories
+src_dir=./dotFiles
+dst_dir=~/test/
+
+# Function to prompt user for confirmation
+confirm() {
+	read -p "Do you want to replace existing configuration files in $dst_dir? (y/n): " response
+	case $response in
+	[yY])
+		return 0
+		;;
+	*)
+		echo "Aborted."
+		exit 1
+		;;
+	esac
+}
+
+# Prompt user for confirmation
+confirm
+
+# Copy files
+cp -r $src_dir/. $dst_dir
+
+echo "Files copied successfully!"
--- termConfigMaker.sh
+++ termConfigMaker.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-src_dir=~
-dst_dir=./dotFiles
+src_dir=/Users/user
+dst_dir=./dotFiles/
 
 # Copy files
 cp $src_dir/.zshrc $dst_dir
 cp $src_dir/.zshenv $dst_dir
-cp -r $src_dir/.oh-my-zsh $dst_dir
-cp -r $src_dir/.tmux $dst_dir
 cp $src_dir/.tmux.conf $dst_dir
-cp -r $src_dir/.conf $dst_dir
+cp -r $src_dir/.config $dst_dir
+
+git add ./dotFiles/
+git commit -m 'updated dot files'
+git push origin main
